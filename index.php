9c588409e360e58baef87db9a2db25ae8c232aa5
Дмитрий Кречетников <dkrech07@gmail.com>
2020-07-29 21:48:27 +0500

    Выводит список объявлений и категорий

--- index.php
+++ index.php
@@ -47,6 +47,40 @@ $user_avatar = 'img/user.jpg';
           </ul>
         <?php endif ?>
 
+        </nav>
+    </div>
+</header>
+
+<main class="container">
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
+            </li>
+            <li class="promo__item promo__item--attachment">
+                <a class="promo__link" href="all-lots.html">Крепления</a>
+            </li>
+            <li class="promo__item promo__item--boots">
+                <a class="promo__link" href="all-lots.html">Ботинки</a>
+            </li>
+            <li class="promo__item promo__item--clothing">
+                <a class="promo__link" href="all-lots.html">Одежда</a>
+            </li>
+            <li class="promo__item promo__item--tools">
+                <a class="promo__link" href="all-lots.html">Инструменты</a>
+            </li>
+            <li class="promo__item promo__item--other">
+                <a class="promo__link" href="all-lots.html">Разное</a>
+            </li>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+
         <?php
           $ads_categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
           $ads_list = [
@@ -89,51 +123,19 @@ $user_avatar = 'img/user.jpg';
           ];
         ?>
 
-        </nav>
-    </div>
-</header>
-
-<main class="container">
-    <section class="promo">
-        <h2 class="promo__title">Нужен стафф для катки?</h2>
-        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
-        <ul class="promo__list">
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>
-        </ul>
-    </section>
-    <section class="lots">
-        <div class="lots__header">
-            <h2>Открытые лоты</h2>
-        </div>
         <ul class="lots__list">
+          <?php foreach ($ads_list as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= $val['URL картинки'] ?>" width="350" height="260" alt="<?= $val['Название'] ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?= $val['Категория'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $val['Название'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $val['Цена'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -141,6 +143,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+          <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -148,24 +151,11 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
+          <?php foreach ($ads_categories as $key => $val): ?>
             <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
+                <a href="all-lots.html"><?= $val ?></a>
             </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+          <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
